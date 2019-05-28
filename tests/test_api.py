e8ea415312799d7480f7d9721659e3c2700ebc6f
Eziz Durdyyev <ezizdurdy@gmail.com>
2019-05-28 07:31:42 +0500

    basic testing with pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,26 @@
+# call: pytest -v test_api.py::TestApi
+
 import pytest
+import requests
+
+url = 'http://127.0.0.1:5000' # The root url of the flask app
+
+class TestApi():
+    def test_get_games(self):
+        response = requests.get(url+'/games')
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data['games'] == 0
+
+    def test_delete_game(self):
+        response = requests.delete(url+'/games/1')
+        data = response.json()
+
+        assert response.status_code == 204
+        assert len(data) == 0
+
+# def test_get_games(client):
+#     response = client.get("/api/games")
+#     assert response.status_code == 200
+#     assert response.data["games"] ==  
