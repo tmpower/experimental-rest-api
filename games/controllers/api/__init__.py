70b3690e0b848a9615c704aaeb7f4531e9e888d2
Eziz Durdyyev <ezizdurdy@gmail.com>
2019-06-07 12:07:40 +0500

    marshall for serialization with reqparse

--- games/controllers/api/__init__.py
+++ games/controllers/api/__init__.py
@@ -2,16 +2,9 @@ from flask_restful import Api
 
 rest_api = Api()
 
-from games.controllers.api.games import GamesAPI
-rest_api.add_resource(
-    GamesAPI,
-    '/api/games',
-    '/api/games/<int:game_id>',
-)
+from games.controllers.api.games import GameAPI, GamesAPI
+rest_api.add_resource( GameAPI, '/api/games/<int:game_id>', endpoint = 'game')
+rest_api.add_resource( GamesAPI, '/api/games', endpoint = 'games')
 
 from games.controllers.api.categories import CategoriesAPI
-rest_api.add_resource(
-    CategoriesAPI,
-    '/api/categories',
-    '/api/categories/<int:category_id>',
-)
+rest_api.add_resource( CategoriesAPI, '/api/categories', '/api/categories/<int:category_id>' )
--- games/controllers/api/games.py
+++ games/controllers/api/games.py
@@ -1,56 +1,59 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, marshal, fields
 from games.models import db, Game
 from flask import jsonify, abort, json
 
 
+game_fields = {
+    'title': fields.String,
+    'developer': fields.String,
+    'category': fields.String,
+    'uri': fields.Url('task')
+}
+
 class GamesAPI(Resource):
-    def get(self, game_id=None):
-        if game_id:
-            # parser = reqparse.RequestParser()
-            # parser.add_argument('id', required=False, type=int, location='args')
-            # args = parser.parse_args(strict=True)
-            # game_id = args.get('id')
-            # if game_id is not None:
-            #     Game.query.get(game_id)
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True, help='No game title provided', location='json')
+        self.reqparse.add_argument('developer_id', type=int, default=1, location='json')
+        self.reqparse.add_argument('category_id', type=int, default=1, location='json')
+        super(GamesAPI, self).__init__()
 
-            return jsonify(Game.query.get_or_404(game_id).to_json())
-        else:
-            return jsonify([game.to_json() for game in Game.query.all()])
+    def get(self):
+        return jsonify([game.to_json() for game in Game.query.all()])
 
+    def post(self):
+        # abort_if_no_admin_auth(token) // extract token from url
+        args = self.reqparse.parse_args(strict=True)
+        new_game = Game(args['title'])
+        db.session.add(new_game)
+        db.session.commit()
 
-    def post(self, game_id=None):
-        if game_id:
-            abort(400)
-        else:
-            # abort_if_no_admin_auth(token) // extract token from url
-
-            new_game = Game()
-            db.session.add(new_game)
-            db.session.commit()
-
-            return {"result" : new_game.id}, 201
+        return {"result" : new_game.id}, 201
 
 
-    def put(self, game_id=None):
-        if not game_id:
-            abort(400)
-        else:
-            # abort_if_no_admin_auth(token)
+class GameAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('developer_id', type=int, location='json')
+        self.reqparse.add_argument('category_id', type=int, location='json')
+        super(GameAPI, self).__init__()
 
-            game = Game.query.get_or_404(game_id)
-            db.session.commit()
+    def get(self, game_id):
+        return jsonify( marshal(Game.query.get_or_404(game_id), game_fields) )
 
-            return {"result" : game.id}, 201
+    def put(self, game_id):
+        # abort_if_no_admin_auth(token)
+        game = Game.query.get_or_404(game_id)
+        # edit and update 'game' here..
+        db.session.commit()
 
+        return {"result" : game.id}, 201
 
-    def delete(self, game_id=None):
-        if not game_id:
-            abort(400)
-        else:
-            # abort_if_no_admin_auth(token)
+    def delete(self, game_id):
+        # abort_if_no_admin_auth(token)
+        game = Game.query.get_or_404(game_id)
+        db.session.delete(game)
+        db.session.commit()
 
-            game = Game.query.get_or_404(game_id)
-            db.session.delete(game)
-            db.session.commit()
-
-            return "", 204
+        return "", 204
