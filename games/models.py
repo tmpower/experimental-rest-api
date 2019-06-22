9955bce90b693962363d56899bab3b81f7d5d2d8
Eziz Durdyyev <ezizdurdy@gmail.com>
2019-06-22 16:18:20 +0500

    bug_fix in User model

--- games/models.py
+++ games/models.py
@@ -51,7 +51,7 @@ class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(32), index = True)
-    password_hash = (db.String(64))
+    password_hash = db.Column(db.String())
 
     ## password handling
     def hash_password(self, password):
